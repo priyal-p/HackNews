5e2673fd58632d8b24c1d17d535f7fa7b070fb68
Priyal PORWAL <26priyalmrbd@gmail.com>
2024-03-12 11:21:17 +0530

    Adds NavigationView, List

--- H4X0R/ContentView.swift
+++ H4X0R/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView(content: {
+            List {
+                NavigationLink(destination: Text("Destination")) { Text("Navigate") }
+            }
+            .navigationTitle("Hack News")
+            .navigationBarTitleDisplayMode(.automatic)
+        })
     }
 }
 
